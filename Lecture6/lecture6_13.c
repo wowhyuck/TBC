1f73eec948abb9374bfa931319f6c904ab672acc
Joohyuck <joohyuck.seo@gmail.com>
2020-08-05 21:54:45 +0900

    Add the code of lecture 6.13
    : 탈출조건 루프 do while

---
+++ Lecture6/lecture6_13.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+
+int main()
+{
+	const secret_code = 337;
+	int guess = 0;
+
+	//printf("Enter secret code : ");
+	//scanf("%d", &guess);
+
+	//while (guess != secret_code)
+	//{
+	//	printf("Enter secret code : ");
+	//	scanf("%d", &guess);
+	//}
+
+	do 
+	{
+		printf("Enter secret code : ");
+		scanf("%d", &guess);
+	} while (guess != secret_code);
+
+	printf("Good!");
+
+	return 0;
+}
+
