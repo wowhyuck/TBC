d4823c5ea50faedbd7e1274e1a2fe676a1d86e58
Joohyuck <joohyuck.seo@gmail.com>
2020-08-05 19:46:19 +0900

    Add the code of lecture 6.12
    : 제논의 역설 시뮬레이션 예제

---
+++ Lecture6/lecture6_12.c
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+
+int main()
+{
+	/*
+		Simulation of a Moving Object
+
+		- Speed = 1.0 m./s
+		- dt = 1.0 s
+		- Distance traveled during dt = Speed * dt
+
+		- Step 1 : 1.0 m/s * 1.0 s = 1.0 m, dist = 1.0 m
+		- Step 2 : 1.0 m/s * 1.0 s = 1.0 m, dist = 1.0 m + 1.0 m = 2.0 m
+		- Step 3 : 1.0 m/s * 1.0 s = 1.0 m, dist = 2.0 m + 1.0 m = 3.0 m
+		- ... ...
+	*/
+
+	/*
+		Zeno's Paradox
+	
+		- Step 1 : 1.0 * 1.0 = 1.0, 1.0
+		- Step 2 : 1.0 * 0.5 = 0.5, 1.0 + 0.5 = 1.5
+		- Step 3 : 1.0 * 0.25 = 0.25, 1.5 + 0.25 = 1.75
+		- ... ...
+	*/
+	const double speed = 1.0;
+	const unsigned repeat_max = 10;
+	
+	double dt = 1.0;
+	double time = 0.0;		// Elapsed time
+	double dist = 0.0;		// Distance
+
+	for (unsigned i = 0; i < repeat_max; ++i)
+	{
+		dist += speed * dt;		// dist = dist + speed * dt
+		time += dt;
+
+		printf("Elapsed time = %fs, ", time);
+		printf("Distance = %fm\n", dist);
+	}
+	printf("\n");
+
+	// Zeno's Paradox
+	time = 0.0;
+	dist = 0.0;
+	for (unsigned i = 0; i < repeat_max; ++i)
+	{
+		dist += speed * dt;		// dist = dist + speed * dt
+		time += dt;
+		printf("Elapsed time = %fs, ", time);
+		printf("Distance = %fm\n", dist);
+
+		dt *= 0.5;
+	}
+
+	// Note: infinite geometric series
+
+	return 0;
+}
