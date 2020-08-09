bba400b88392a7d90b121b7564c191877f78cc60
Joohyuck <joohyuck.seo@gmail.com>
2020-08-09 18:52:55 +0900

    Add the code of lecture 7.9
    : 조건 연산자

---
+++ Lecture7/lecture7_9.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main()
+{
+	//int temp;
+	//temp = true ? 1024 : 7;		// ternary
+	//printf("%d\n", temp);
+
+	//temp = false ? 1024 : 7;
+	//printf("%d\n", temp);
+
+	/*---------------------------------------------*/
+
+	//int number;
+	//scanf("%d", &number);
+
+	//bool is_even;
+
+	////if (number % 2 == 0)
+	////	is_even = true;
+	////else
+	////	is_even = false;
+
+	//is_even = (number % 2 == 0) ? true : false;
+
+	//if (is_even)
+	//	printf("Even\n");
+	//else
+	//	printf("Odd\n");
+
+	/*---------------------------------------------*/
+
+	int a = 1, b = 2;
+	int max = (a > b) ? a : b;
+
+	return 0;
+}
