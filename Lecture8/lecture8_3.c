988b39a8bd7f57c933e4050a4e3c3a20823cf24f
Joohyuck <joohyuck.seo@gmail.com>
2020-08-10 16:38:27 +0900

    Add the code of lecture 8.3
    : 입출력 방향 재지정 using cmd

---
+++ Lecture8/lecture8_3.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+
+int main()
+{
+	//printf("Programming");
+
+	char str[100];
+	scanf("%s", str);
+	printf("I love %s\n", str);
+
+	return 0;
+}
