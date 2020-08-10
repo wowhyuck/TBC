2241955ed3057649b5174a64b5fb75c93bd63f96
Joohyuck <joohyuck.seo@gmail.com>
2020-08-10 16:19:44 +0900

    Add the code of lecture 8.1
    : 입출력 버퍼

---
+++ Lecture8/lecture8_1.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <conio.h>		//Windows, _getch(), _getche()
+
+
+int main()
+{
+	char c;
+
+	//while ((c = getchar()) != '.')
+	//	putchar(c);
+
+	while ((c = _getche()) != '.')		// e <- echo
+		putchar(c);
+
+	return 0;
+}
