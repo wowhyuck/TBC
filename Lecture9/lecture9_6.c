a4263343d6a3a7bf11dd2fafaa990cdb237de22d
Joohyuck <joohyuck.seo@gmail.com>
2020-08-12 02:16:28 +0900

    Add the code of lecture 9.6
    : 재귀 호출

---
+++ Lecture9/lecture9_6.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+void my_func(int n);
+
+
+int main()
+{
+	my_func(1);
+
+	return 0;
+}
+
+
+void my_func(int n)
+{
+	printf("Level %d, address of variable n = %p\n", n, &n);
+
+	// TODO: stop condition
+	if(n < 4)
+		my_func(n + 1);
+
+	printf("Level %d, address of variable n = %p\n", n, &n);
+
+	// TODO: tail recursion
+
+}
