872647727b9899a2772fea5869b1d66e069103e5
Joohyuck <joohyuck.seo@gmail.com>
2020-08-16 03:00:30 +0900

    Add the code of lecture 11.2
    : 매모리 레이아웃과 문자열

---
+++ Lecture11/lecture11_2.c
@@ -0,0 +1,101 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>			// malloc
+
+void test_function()
+{
+	int j;
+	printf("Stack high \t%llu\n", (unsigned long long) & j);
+}
+
+
+int main()
+{
+	/* Array vs Pointer */
+	const char* pt2 = "I am a string!.";
+	const char* pt3 = "I am a string!.";
+	const char* pt4 = "I am a string!!!!!!!!!!!!!!!.";		// <- different
+	const char arr1[] = "I am a string!.";
+	const char arr2[] = "I am a string!.";
+	const char arr3[] = "I am a string!!!!!.";			// <- different
+
+	printf("rodata low \t%llu %llu %llu %llu\n",
+		(unsigned long long) pt2, (unsigned long long) pt3,
+		(unsigned long long) pt4, (unsigned long long) "I am a string!.");
+
+	printf("Stack high \t%llu %llu %llu\n",
+		(unsigned long long) arr1, (unsigned long long) arr2,
+		(unsigned long long) arr3);				// check address numbers!
+
+	/* Memory address check */
+
+	/* Local variables */
+	printf("Stack high \t%llu %llu %llu\n",
+		(unsigned long long) &pt2, (unsigned long long) &pt3, (unsigned long long) & pt4);
+
+	int i;
+	printf("Stack high \t%llu\n", (unsigned long long) & i);
+
+	/* Local variable in a function */
+	test_function();
+
+	/* Dynamic allocation */
+	char* p5 = (char*)malloc(sizeof(char) * 100);
+	printf("Heap middle \t%llu\n", (unsigned long long)p5);
+
+	char* p6 = (char*)malloc(sizeof(char) * 100);
+	printf("Heap middle \t%llu\n", (unsigned long long)p6);
+
+	/* Array vs Pointer (continued) */
+	//arr1++;				// Error
+	pt2++;					// OK
+	puts(pt2);				// Check the first character
+
+	/* Array and Pointer Differences */
+	char heart[] = "I love Kelly!";
+	const char* head = "I love Helly!";
+
+	for (int i = 0; i < 6; i++)
+		putchar(heart[i]);				// Note: putchar()
+	putchar('\n');
+
+	for (int i = 0; i < 6; i++)
+		putchar(head[i]);				// Note: putchar()
+	putchar('\n');
+
+	// pointer addition
+	for (int i = 0; i < 6; i++)
+		putchar(*(head + i));			
+	putchar('\n');
+
+	while (*(head) != '\0')
+		putchar(*(head++));
+	putchar('\n');
+
+	head = heart;
+	//heart = head;		// Error
+
+	// Cannot change heart. Can change the elements of heart
+	heart[7] = 'H';
+	*(heart + 7) = 'K';
+
+	putchar('\n');
+
+	char* word = "Gogle";
+	//word[2] = 'o';		// Run-time error!
+	puts(word);
+
+	// Note: const char* word = "Goggle"; is recommended
+
+	const char* str1 = "When all the lights are low, ...";
+	const char* copy;
+
+	copy = str1;
+
+	printf("%s %p %p\n", str1, str1, &str1);
+	printf("%s %p %p\n", copy, copy, &copy);
+
+	// Note: strcpy(), strncpy()
+
+	return 0;
+}
