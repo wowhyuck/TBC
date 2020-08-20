4596c36e5d4865f7ffaf1eb4edc3f5db74c50d36
Joohyuck <joohyuck.seo@gmail.com>
2020-08-21 00:53:29 +0900

    Add the code of lecture 12.16
    : calloc(), realloc()

---
+++ Lecture12/lecture12_16.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	int n = 10;
+
+	int* ptr = NULL;
+
+	//ptr = (int*)malloc(sizeof(int) * n);
+	ptr = (int*)calloc(n, sizeof(int));			// contiguous alloction
+	if (!ptr)
+		exit(1);
+
+	for (int i = 0; i < n; i++)					// calloc 함수는 초기화를 해줌. 출력하면 0
+		printf("%d ", ptr[i]);					// malloc 함수는 초기화를 안해주기 때문에 쓰레기값이 출력
+	printf("\n");
+
+	/*
+		realloc() KNK p. 422
+		- doesn't initialize the bytes added
+		- returns NULL if can't enlarger the memory block
+		- If first argument is NULL, it behaves like malloc()
+		- If second argument is 0, it frees the memory block
+	*/
+
+	for (int i = 0; i < n; i++)
+		ptr[i] = i + 1;
+
+	n = 20;
+
+	int* ptr2 = NULL;
+	ptr2 = (int*)realloc(ptr, n * sizeof(int));
+	//ptr = (int*)realloc(ptr, n * sizeof(int));
+
+	printf("%p %p\n", ptr, ptr2);
+	printf("%d\n", ptr[0]);
+
+	if (!ptr2)
+		exit(1);
+	else
+		ptr = NULL;
+
+	for (int i = 0; i < n; i++)					
+		printf("%d ", ptr2[i]);					
+	printf("\n");
+
+	return 0;
+}
