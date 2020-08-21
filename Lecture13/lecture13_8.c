7e728a2d029689a9736f4cb0b0bbca25f1b4755e
Joohyuck <joohyuck.seo@gmail.com>
2020-08-22 00:53:47 +0900

    Add the code of lecture 13.8
    : 텍스트 파일을 바이너리처럼 읽어보기

---
+++ Lecture13/lecture13_8.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>			// SetConsoleOutputCP()
+
+
+int main()
+{
+	FILE* fp = fopen("test.txt", "rb");
+
+	unsigned char ch;
+
+	SetConsoleOutputCP(CP_UTF8);		// UTF-8 mode
+
+	while (fread(&ch, sizeof(unsigned char), 1, fp) > 0)
+	{
+		printf("%hhu %c\n", ch, ch);
+	}
+
+	fclose(fp);
+
+	return 0;
+}
