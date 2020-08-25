307cc5d33ea28ac380365b6cb309ce841839d53e
Joohyuck <joohyuck.seo@gmail.com>
2020-08-25 17:32:12 +0900

    Add the code of lecture 14.5
    : 구조체를 다른 구조체의 멤버로 사용하기

---
+++ Lecture14/lecture14_5.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#define LEN 20
+
+struct names {				// A structure
+	char given[LEN];		// first name
+	char family[LEN];		// last name
+};
+
+struct reservation {		// Another structure
+	struct names guest;		// a nested structure
+	struct names host;		// one more nested structure
+	char food[LEN];
+	char place[LEN];
+
+	// time
+	int year;
+	int month;
+	int day;
+	int hours;
+	int minutes;
+};
+
+
+int main()
+{
+	struct reservation res = {
+		.guest = {"Nick", "Carraway"},
+		.host = {"Jay", "Gatsby"},
+		.place = {"the Gatsby mansion"},
+		.food = {"Escargot"},
+		.year = 1925,
+		.month = 4,
+		.day = 10,
+		.hours = 18,
+		.minutes = 30
+	};
+
+	/*
+	Dear Nick Carraway,
+	I would like to serve you Escargot.
+	Please visit the Gatsby mansion on 10/4/1925 at 18:30.
+	Sincerely,
+	Jay Gatsby
+	*/
+
+	printf("Dear %s %s,\n", res.guest.given, res.guest.family);
+	printf("I would like to serve you %s.\n", res.food);
+	printf("Please visit %s on %d/%d/%d at %d:%d.\n", 
+		res.place, res.day, res.month, res.year, res.hours, res.minutes);
+	printf("Sincerely,\n%s %s", res.host.given, res.host.family);
+
+	return 0;
+}
+
+
+
+
+
+
