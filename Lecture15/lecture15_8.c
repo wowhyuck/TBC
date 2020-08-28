5ec70e33728d680e74dd9670c4898d62d303a868
Joohyuck <joohyuck.seo@gmail.com>
2020-08-28 22:36:06 +0900

    Add the code of lecture 15.8
    : RGBA 색상 비트 마스크 연습문제

---
+++ Lecture15/lecture15_8.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#define BYTE_MASK 0xff
+
+int main()
+{
+	unsigned int rgba_color = 0x66CDAAFF;
+	// 4 bytes, medium aqua marine (102, 205, 170, 255)
+
+	unsigned char red, green, blue, alpha;
+
+	// Use right shift >> operator
+	alpha	= (rgba_color & BYTE_MASK);	// TODO:
+	blue	= (rgba_color >> 8 & BYTE_MASK);	// TODO:
+	green	= (rgba_color >> 16 & BYTE_MASK);	// TODO:
+	red		= (rgba_color >> 24 & BYTE_MASK);	// TODO:
+
+	printf("(R, G, B, A) = (%hhu, %hhu, %hhu, %hhu)\n",
+		red, green, blue, alpha);
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
+
+
+
+
