c8cf98f0ea6d8e4c630a4154ae32602dc6928d40
Joohyuck <joohyuck.seo@gmail.com>
2020-09-04 17:50:12 +0900

    Add the code of lecture 17.9
    : 스택 자료구조

---
+++ Lecture17/stack.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "element.h"
+#include <stdbool.h>
+
+#define MAX_STACK_SIZE 5
+
+typedef struct stack
+{
+	element items[MAX_STACK_SIZE];
+	int top;			// index of the element added lastly
+} Stack;
+
+void Initialize(Stack* stack);
+bool isFull(const Stack* stack);
+bool isEmpty(const Stack* stack);
+void Push(Stack* stack, element item);
+element Pop(Stack* stack);
